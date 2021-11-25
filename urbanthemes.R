64adfa95c004be00a9f0f6945ae26a99e527453c
Nathan Nguyen <58866415+toasted-marshmallow@users.noreply.github.com>
2021-11-25 02:36:15 -0700

    Add files via upload

--- urbanthemes.R
+++ urbanthemes.R
@@ -3,16 +3,19 @@ rm(list = ls())
 # https://urbaninstitute.github.io/r-at-urban/graphics-guide.html
 # install.packages("devtools")
 # devtools::install_github("UrbanInstitute/urbnthemes")
+# https://stackoverflow.com/questions/40980189/removing-latitude-and-longitude-labels-in-ggplot
 
 library(tidyverse)
 library(urbnmapr)
 library(scales)
 library(urbnthemes)
+library(ggthemes)
+library(plotly)
 
 set_urbn_defaults(style = "print")
 
 
-countydata %>% 
+p1 <- countydata %>% 
   left_join(counties, by = "county_fips") %>% 
   filter(state_name =="California") %>% 
   ggplot(mapping = aes(long, lat, group = group, fill = horate)) +
@@ -23,9 +26,19 @@ countydata %>%
   theme(legend.title = element_text(),
         legend.key.width = unit(.5, "in")) +
   labs(fill = "Homeownership rate") +
-  theme_urbn_map()
+  theme(axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) 
 
+ggsave("ca_plot.jpg", plot = p1)
+  # theme_urbn_map()
 
+# ggplotly(p1)
 household_data <- left_join(countydata, counties, by = "county_fips") 
 
 household_data %>%
@@ -34,17 +47,27 @@ household_data %>%
   coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
   labs(fill = "Median Household Income")
 
-require(urbnthemes)
 household_data %>%
   ggplot(aes(long, lat, group = group, fill = horate)) +
   geom_polygon(color = NA) +
   urbnthemes::scale_fill_gradientn(labels = scales::percent,
-                       guide = guide_colorbar(title.position = "top")) +
+                       guide = guide_colorbar(title.position = "bottom")) +
   geom_polygon(data = states, mapping = aes(long, lat, group = group),
                fill = NA, color = "#ffffff") +
   coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
   theme(legend.title = element_text(),
         legend.key.width = unit(.5, "in")) +
   labs(fill = "Homeownership rate") +
-  theme_urbn_map()
+  # hide original axis labels
+  theme(axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) 
+  # theme_map() + theme(legend.position = "top")
+  # theme_urbn_map()
+  # theme(legend.position = "top")
 
